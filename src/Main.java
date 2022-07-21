2ee54bf393827e8edeb7ea54beaba8acaa8042dc
Konstantin Gunko <Ray99999@yandex.ru>
2022-07-21 20:18:40 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SalesManager day1 = new SalesManager(new int[]{35, 38, 36, 40, 39, 45, 43});
+
+        System.out.println("Максимальное количество проданных позиций в течение дня " + day1.max() + " шт");
+    }
+}
